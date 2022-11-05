e82f9397dab9a803b9f41954a9876e9ab8587868
Wako <97005647+wako1203@users.noreply.github.com>
2022-11-05 17:19:16 +0700

    update 5/11/2022

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,29 +12,32 @@ use App\Models\Episode;
 class IndexController extends Controller
 {
     public function home(){
-        $category = Category::orderBy('id','DESC')->get();
+        $category = Category::orderBy('id','DESC')->where('status',1)->get();
         $genre = Genre::orderBy('id','DESC')->get();
         $country = Country::orderBy('id','DESC')->get();
         return view('Clients.Home', compact('category', 'genre', 'country'));
     }
 
     public function category($slug){
-        $category = Category::orderBy('id','DESC')->get();
+        $category = Category::orderBy('id','DESC')->where('status',1)->get();
         $genre = Genre::orderBy('id','DESC')->get();
         $country = Country::orderBy('id','DESC')->get();
-        return view('Clients.Category', compact('category', 'genre', 'country'));
+        $cate_slug = Category::where('slug',$slug)->first();
+        return view('Clients.Category', compact('category', 'genre', 'country', 'cate_slug'));
     }
     public function genre($slug){
-        $category = Category::orderBy('id','DESC')->get();
+        $category = Category::orderBy('id','DESC')->where('status',1)->get();
         $genre = Genre::orderBy('id','DESC')->get();
         $country = Country::orderBy('id','DESC')->get();
-        return view('Clients.Genre', compact('category', 'genre', 'country'));
+        $gen_slug = Genre::where('slug',$slug)->first();
+        return view('Clients.Genre', compact('category', 'genre', 'country','gen_slug'));
     }
     public function country($slug){
-        $category = Category::orderBy('id','DESC')->get();
+        $category = Category::orderBy('id','DESC')->where('status',1)->get();
         $genre = Genre::orderBy('id','DESC')->get();
         $country = Country::orderBy('id','DESC')->get();
-        return view('Clients.Country', compact('category', 'genre', 'country'));
+        $coun_slug = Country::where('slug',$slug)->first();
+        return view('Clients.Country', compact('category', 'genre', 'country', 'coun_slug'));
     }
     public function movie(){
         return view('Clients.Movie');
